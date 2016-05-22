9f800bf0a40bf972defb6e5478385a8d72e99be1
Richa Dighe <richadighe@gmail.com>
2016-05-21 17:23:24 -0700

    Program to print numbers

---
+++ src/PrintNumbers.java
@@ -0,0 +1,10 @@
+
+public class PrintNumbers {
+	public static void main(String[] args) {
+		System.out.println("Program to print the numbers from 1 to 100");
+		for (int i = 1; i <= 100; i++)
+			System.out.println(i);
+
+	}
+
+}
