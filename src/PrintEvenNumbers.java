3f1f660c583a2b53a9c7e2df4f726bd2f6c2dd03
Richa Dighe <richadighe@gmail.com>
2016-05-21 17:52:37 -0700

    Program cosmetic changes

--- src/PrintEvenNumbers.java
+++ src/PrintEvenNumbers.java
@@ -1,7 +1,7 @@
 
 public class PrintEvenNumbers {
 	public static void main(String[] args) {
-		System.out.println("program to print even numbers. \n List of even numbers from 1 to 100 are ");
+		System.out.println("program to print even numbers. " + "\n List of even numbers from 1 to 100 are ");
 		for (int i = 1; i <= 100; i++) {
 			if (i % 2 == 0)
 				System.out.println(i);
